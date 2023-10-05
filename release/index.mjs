f8daafb8893c804cf3954c03c647873bd00b76b3
Ryo.Ogawa <ryo.ogawa@access-company.com>
2023-10-05 12:23:32 +0900

    Squashed commit of the following:
    commit edef3bbb34aade93f2cf58d51272ae2cdcc11178
    Author: Ryo.Ogawa <ryo.ogawa@access-company.com>
    Date:   Thu Oct 5 12:22:57 2023 +0900
    
        fix
    
    commit 964aa4bce5ab5e65bcb59721674ef40b2d193046
    Author: Ryo.Ogawa <ryo.ogawa@access-company.com>
    Date:   Thu Oct 5 12:17:00 2023 +0900
    
        fix
    
    commit f4b27e6168c4570edd48fdd236c05ed432bb45a3
    Author: Ryo.Ogawa <ryo.ogawa@access-company.com>
    Date:   Thu Oct 5 12:09:54 2023 +0900
    
        fix
    
    commit 68590567a0722577735175e7463e0b5e7eca6021
    Author: Ryo.Ogawa <ryo.ogawa@access-company.com>
    Date:   Thu Oct 5 11:47:55 2023 +0900
    
        fix

--- release/index.mjs
+++ release/index.mjs
@@ -20,29 +20,42 @@ try {
       },
     ],
   });
-  console.log(releaseType);
 
-  const isMergeOk = await confirm({ message: "Confirm " });
-  if (!isMergeOk) exit(0);
+  const releaseOk = await confirm({
+    message: `Release ${releaseType} version`,
+    default: false,
+  });
+  if (!releaseOk) process.exit(0);
 
-  console.log("[TODO] merge");
+  let output;
 
-  // execSync("git fetch origin release && git checkout -B release");
-  // execSync("git checkout main && git pull origin main");
-  // execSync("git merge --squash release");
+  console.info("fetch and checkout");
+  output = execSync("git fetch origin release && git checkout -B release");
+  console.log(output.toString());
+  output = execSync("git checkout main && git pull origin main");
+  console.log(output.toString());
 
-  const tagName = execSync(`cd .. && npm version ${releaseType}`)
-    .toString()
-    .trim();
+  console.info("merge and add tag");
+  output = execSync("git merge --squash release && git commit");
+  console.log(output.toString());
+  output = execSync(`cd .. && npm version ${releaseType}`);
+  const tagName = output.toString().trim();
+  console.log(tagName);
 
-  // execSync(`git push origin main`);
-  // execSync(`git push origin ${tagName}`);
+  console.info("remote push");
+  output = execSync(`git push origin main`);
+  console.log(output.toString());
+  output = execSync(`git push origin ${tagName}`);
+  console.log(output.toString());
 
-  // execSync(`git branch -d release`);
-  // execSync(`git push origin --delete release`);
+  console.info("delete release branch");
+  output = execSync(`git branch -d release`);
+  console.log(output.toString());
+  output = execSync(`git push origin --delete release`);
+  console.log(output.toString());
 
-  console.info("release success");
+  console.info("Release Success");
 } catch (err) {
-  console.error(err);
-  console.error("release failed");
+  console.error("Release Failed");
+  console.error(err.toString());
 }
