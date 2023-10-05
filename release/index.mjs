3a76907e94dbb715846b4039f1e65950b17dd1aa
Ryo.Ogawa <ryo.ogawa@access-company.com>
2023-10-05 16:59:17 +0900

    Squashed commit of the following:
    commit af7b4343b2d5ac6bca12ac927ef4732f0b22d814
    Author: Ryo.Ogawa <ryo.ogawa@access-company.com>
    Date:   Thu Oct 5 16:58:42 2023 +0900
    
        fix script
    
    commit d127981a29f002f0a951a2ce84f8717199cc3625
    Author: Ryo.Ogawa <ryo.ogawa@access-company.com>
    Date:   Thu Oct 5 16:08:02 2023 +0900
    
        fix
    
    commit d27171d3afe08d03b28f64ccd861a4d9dcfdb5f5
    Author: Ryo.Ogawa <ryo.ogawa@access-company.com>
    Date:   Thu Oct 5 16:00:00 2023 +0900
    
        test

--- release/index.mjs
+++ release/index.mjs
@@ -3,9 +3,16 @@ import { confirm } from "@inquirer/prompts";
 import { select } from "@inquirer/prompts";
 import chalk from "chalk";
 
-const log = (output) => console.log(chalk.white(output.toString()));
-const info = (message) => console.info(chalk.green(message));
-const error = (message) => console.error(chalk.red(message));
+const log = (msg) => console.log(chalk.white(msg));
+const info = (msg) => console.info(chalk.green(msg));
+const error = (msg) => console.error(chalk.red(msg));
+
+const cmdExec = (cmd) => {
+  log(cmd);
+  const output = execSync(cmd).toString().trim();
+  log(output);
+  return output;
+};
 
 try {
   const releaseType = await select({
@@ -34,24 +41,23 @@ try {
   if (!releaseOk) process.exit(0);
 
   info("--- Fetch and Checkout ---");
-  log(execSync("git fetch origin release && git checkout -B release"));
-  log(execSync("git checkout main && git pull origin main"));
+  cmdExec("git fetch origin release && git checkout -B release");
+  cmdExec("git checkout main && git pull origin main");
 
   info("--- Merge and Add tag ---");
-  log(execSync("git merge --squash release && git commit --no-edit"));
-  const tagName = execSync(`cd .. && npm version ${releaseType}`)
-    .toString()
-    .trim();
+  cmdExec("git merge --squash release && git commit --no-edit");
+  const tagName = cmdExec(`cd .. && npm version ${releaseType}`);
 
   info("--- Push to remote ---");
-  log(execSync(`git push origin main`));
-  log(execSync(`git push origin ${tagName}`));
+  cmdExec(`git push origin main`);
+  cmdExec(`git push origin ${tagName}`);
 
   info("--- Delete release branch ---");
-  log(execSync(`git branch -D release`));
-  log(execSync(`git push origin --delete release`));
+  cmdExec(`git branch -D release`);
+  cmdExec(`git push origin --delete release`);
 
   info("Release Success!!");
 } catch (err) {
+  error(err);
   error("Release Failed");
 }
